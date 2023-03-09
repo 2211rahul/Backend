4f4f90ffc5220552f81926744b4c6a29eec4407e
unknown <rahulraut7588@gmail.com>
2023-03-09 16:13:08 +0530

    New Commit

--- Server/Routes/Forgotpass.js
+++ Server/Routes/Forgotpass.js
@@ -25,7 +25,7 @@ route.post('/Changepass', (req, res) => {
 //              providing link to mail which will be containt only 10 min
 //             const token = jwt.sign(email,signature,{expiresIn:"10m"});
             const token = jwt.sign(email,signature);
-            const link =`https://cdac-project.onrender.com/Reset/viewnewpasspage/${token}`
+            const link =`http://localhost:5000/Reset/viewnewpasspage/${token}`
             const rt = sendmail(email,link);
             res.send("sucess");
         } 
@@ -59,7 +59,8 @@ route.get("/viewnewpasspage/:token",(req,resp)=>{
 //Update  
 route.post('/resetpassword', (req, res) => {
     const {email,password}=req.body;
-    User.updateOne({ email: email }, { $set: { password: password } }, { new: true }).then(() => res.redirect( 301,"https://cdac-project.onrender.com/login")).catch(err =>  res.send(err));
+    console.log("inside a ");
+    User.updateOne({ email: email }, { $set: { password: password } }, { new: true }).then(() => res.send("updated Successfully")).catch(err =>  res.send(err));
 
 })
 
